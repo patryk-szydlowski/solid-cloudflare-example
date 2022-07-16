72008d866aeb751cb258040313d1e5e3b2f11c7a
Patryk Szydłowski <patrykszydlowski99@gmail.com>
2022-07-16 18:57:55 +0200

    chore: add support for Cloudflare deployment

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,8 @@
 import solid from "solid-start";
+// @ts-expect-error: Solid Cloudflare adapter has no types
+import cloudflare from "solid-start-cloudflare-pages";
 import { defineConfig } from "vite";
 
 export default defineConfig({
-  plugins: [solid()],
+  plugins: [solid({ adapter: cloudflare(), ssr: true })],
 });
