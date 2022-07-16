6f8f061cffe867f8da2146182e6c26a14cfbdef4
Patryk Szydłowski <patrykszydlowski99@gmail.com>
2022-07-16 15:52:43 +0200

    chore: add basic ESLint support

--- src/root.tsx
+++ src/root.tsx
@@ -1,28 +1,26 @@
 // @refresh reload
-import { Links, Meta, Scripts } from "solid-start/root";
-import { ErrorBoundary } from "solid-start/error-boundary";
 import { Suspense } from "solid-js";
 import { Route, Routes } from "solid-start/data";
+import { ErrorBoundary } from "solid-start/error-boundary";
+import { Links, Meta, Scripts } from "solid-start/root";
 
-export default function Root() {
-  return (
-    <html lang="en">
-      <head>
-        <meta charset="utf-8" />
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <Meta />
-        <Links />
-      </head>
-      <body>
-        <ErrorBoundary>
-          <Suspense>
-            <Routes>
-              <Route path="/" element={<div>Hello world!</div>} />
-            </Routes>
-          </Suspense>
-        </ErrorBoundary>
-        <Scripts />
-      </body>
-    </html>
-  );
-}
+export default () => (
+  <html lang="en">
+    <head>
+      <meta charset="utf8" />
+      <meta content="width=device-width, initial-scale=1" name="viewport" />
+      <Meta />
+      <Links />
+    </head>
+    <body>
+      <ErrorBoundary>
+        <Suspense>
+          <Routes>
+            <Route element={<div>Hello world!</div>} path="/" />
+          </Routes>
+        </Suspense>
+      </ErrorBoundary>
+      <Scripts />
+    </body>
+  </html>
+);
