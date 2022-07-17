beb8e019f0db77fb091969668b2ddcc1061d6335
Patryk Szydłowski <patrykszydlowski99@gmail.com>
2022-07-17 23:37:07 +0200

    chore: actually fix vanilla-extract plugin

--- vite.config.ts
+++ vite.config.ts
@@ -5,5 +5,8 @@ import cloudflare from "solid-start-cloudflare-pages";
 import { defineConfig } from "vite";
 
 export default defineConfig({
-  plugins: [solid({ adapter: cloudflare(), ssr: true }), vanillaExtractPlugin()],
+  plugins: [
+    solid({ adapter: cloudflare(), ssr: true }),
+    vanillaExtractPlugin({ identifiers: "debug" }),
+  ],
 });
